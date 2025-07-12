4b166a3a05d52f2d811ee9ea8479df93ea73bd11
Sanju khetavath <sanju@Sanjus-MacBook-Pro.local>
2025-07-12 17:04:41 +1000

    updated code

--- infra/providers.tf
+++ infra/providers.tf
@@ -7,9 +7,9 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "terraform-statefiles"
-    storage_account_name = "terraformremotestatefile"
-    container_name       = "myobcontainer"
+    resource_group_name  = "rg-platform-dev"
+    storage_account_name = "tfrmtstatefile"
+    container_name       = "tfcontainer"
     key                  = "terraform.tfstate"
   }
 }
