a31f580e546879afb2e9fbcfa8dc58d8aa8f2edf
Sanju khetavath <sanju@Sanjus-MacBook-Pro.local>
2025-07-12 17:29:18 +1000

    updated code

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.96.0"
+      version = "=4.1.0"
     }
   }
   backend "azurerm" {
@@ -15,8 +15,4 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }   
